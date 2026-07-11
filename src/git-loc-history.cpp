413e5a7dc59f92bc81d9b4365f552e9df7212eca
Liam Ralph <liamralph@tutamail.com>
2026-07-11 09:49:41 -0400

    add copyright and LICENSE

--- src/git-loc-history.cpp
+++ src/git-loc-history.cpp
@@ -1,3 +1,17 @@
+/*
+Copyright (C) 2026 Liam Ralph
+https://github.com/liam-ralph
+
+This program, including this file, is licensed under the
+MIT/Expat License. See LICENSE or this project's source
+for more information.
+Project Source: https://github.com/liam-ralph/git-loc-history
+
+Git LoC History - An application for view a git repo's history
+in lines of code.
+*/
+
+
 // Includes
 
 #include <iostream>
