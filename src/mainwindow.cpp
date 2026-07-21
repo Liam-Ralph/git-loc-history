f586ef89bb12b36d7a36a3330b134437e288733c
Liam Ralph <liamralph@tutamail.com>
2026-07-21 13:53:39 -0400

    add to gui layout

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,7 @@
 // Includes
 
+#include <QCheckBox>
+#include <QGraphicsView>
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QProgressBar>
@@ -8,7 +10,7 @@
 #include <QVBoxLayout>
 #include <QWidget>
 
-#include <QtGraphs>
+#include <QtCharts>
 
 #include "mainwindow.h"
 
@@ -21,12 +23,39 @@ MainWindow::MainWindow() {
     QWidget *window = new QWidget();
     QVBoxLayout *layout_back = new QVBoxLayout(window);
 
+    // Top
+
     QPushButton *info_button = new QPushButton("Info");
     QTextEdit *path_entry = new QTextEdit("Enter Git repo path");
+
+    // Middle
+    
     QHBoxLayout *layout_middle = new QHBoxLayout();
+
+    QVBoxLayout *layout_excluded_paths = new QVBoxLayout();
+    QTextEdit *excluded_paths_entry = new QTextEdit();
+    layout_excluded_paths->addWidget(new QLabel("Excluded Paths"));
+    layout_excluded_paths->addWidget(excluded_paths_entry);
+
+    QGraphicsView *results_view = new QGraphicsView();
+
+    QVBoxLayout *layout_options = new QVBoxLayout();
+    QCheckBox *progress_check = new QCheckBox("Show Progress");
+    layout_options->addWidget(new QLabel("Options"));
+    layout_options->addWidget(progress_check);
+
+    layout_middle->addLayout(layout_excluded_paths);
+    layout_middle->addWidget(results_view);
+    layout_middle->addLayout(layout_options);
+
+    // Bottom
+
     QProgressBar *progress_bar = new QProgressBar();
     QLabel *timer_label = new QLabel("0.0");
+
     QHBoxLayout *layout_bottom = new QHBoxLayout();
+    layout_bottom->addWidget(new QLabel("Git LoC History"));
+    layout_bottom->addWidget(new QLabel("v"));
 
     layout_back->addWidget(info_button);
     layout_back->addWidget(path_entry);
