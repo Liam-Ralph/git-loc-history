a8efd0b5a6771bfa742ee47623fdbd328f6be594
Liam Ralph <liamralph@tutamail.com>
2026-08-06 09:14:41 -0400

    update build.sh

--- pkg/build.sh
+++ pkg/build.sh
@@ -8,27 +8,32 @@ if [ ! -e "libgit2-1.9.6" ]; then
     rm -f v1.9.6.tar.gz
 fi
 
-sed -i -e "s/#define ABSOLUTE_PATHS false/#define ABSOLUTE_PATHS true/g" src/definitions.cpp
-if [ -e "build" ]; then
-    rm -rf build/*
-else
-    mkdir build
+if [ ! -e "build/git-loc-history" ] || [ ! -e "build/git-loc-history-cli" ]; then
+    sed -i -e "s/#define ABSOLUTE_PATHS false/#define ABSOLUTE_PATHS true/g" src/definitions.cpp
+    if [ -e "build" ]; then
+        rm -rf build/*
+    else
+        mkdir build
+    fi
+    cd build
+    cmake ..
+    cmake --build . --parallel $(nproc) --config Release
+    strip git-loc-history git-loc-history-cli
+    cd ..
+    sed -i -e "s/#define ABSOLUTE_PATHS true/#define ABSOLUTE_PATHS false/g" src/definitions.cpp
 fi
-cd build
-cmake ..
-cmake --build . --parallel $(nproc) --config Release
-strip git-loc-history git-loc-history-cli
-cd ..
-sed -i -e "s/#define ABSOLUTE_PATHS true/#define ABSOLUTE_PATHS false/g" src/definitions.cpp
 
-mkdir usr
-install -Dm755 build/git-loc-history -t pkg/usr/bin/git-loc-history
-install -Dm755 build/git-loc-history-cli -t pkg/usr/bin/git-loc-history-cli
-install -Dm644 pkg/resources/git-loc-history.desktop -t \
+if [ -e "pkg/usr" ]; then
+    rm -rf pkg/usr
+fi
+mkdir pkg/usr
+install -Dm755 build/git-loc-history pkg/usr/bin/git-loc-history
+install -Dm755 build/git-loc-history-cli pkg/usr/bin/git-loc-history-cli
+install -Dm644 pkg/resources/git-loc-history.desktop \
     pkg/usr/share/applications/git-loc-history.desktop
-install -Dm644 pkg/resources/copyright -t pkg/usr/share/doc/git-loc-history/copyright
-install -Dm644 README.md -t pkg/usr/share/doc/git-loc-history/README.md
-install -Dm644 CHANGELOG.md -t pkg/usr/share/doc/git-loc-history/CHANGELOG.md
-install -Dm644 logo.png -t pkg/usr/share/icons/hicolor/512x512/git-loc-history.png
-install -Dm644 LICENSE -t pkg/usr/share/licenses/git-loc-history/LICENSE
+install -Dm644 pkg/resources/copyright pkg/usr/share/doc/git-loc-history/copyright
+install -Dm644 README.md pkg/usr/share/doc/git-loc-history/README.md
+install -Dm644 CHANGELOG.md pkg/usr/share/doc/git-loc-history/CHANGELOG.md
+install -Dm644 logo.png pkg/usr/share/icons/hicolor/512x512/git-loc-history.png
+install -Dm644 LICENSE pkg/usr/share/licenses/git-loc-history/LICENSE
 
