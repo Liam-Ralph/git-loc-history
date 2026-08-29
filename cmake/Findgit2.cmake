71eacb3e4763e2e0c55b072e922bb3e218f92723
Liam Ralph <liamralph@tutamail.com>
2026-08-29 09:06:55 -0400

    move libgit2 dependencies to Findgit2.cmake, add openssl dependency to packages

--- cmake/Findgit2.cmake
+++ cmake/Findgit2.cmake
@@ -1,3 +1,6 @@
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(OPENSSL REQUIRED openssl)
+
 find_path(GIT2_INCLUDE_PATH NAMES git2.h HINTS "${CMAKE_SOURCE_DIR}/libgit2-1.9.7/build/include")
 find_library(GIT2_LIBRARY NAMES libgit2.a git2 HINTS "${CMAKE_SOURCE_DIR}/libgit2-1.9.7/build")
 
@@ -7,7 +10,7 @@ find_package_handle_standard_args(git2 REQUIRED_VARS GIT2_LIBRARY GIT2_INCLUDE_P
 if (GIT2_FOUND)
     set(GIT2_INCLUDE_DIR ${GIT2_INCLUDE_PATH})
     set(GIT2_INCLUDE_DIRS ${GIT2_INCLUDE_PATH})
-    set(GIT2_LIBRARIES ${GIT2_LIBRARY})
+    set(GIT2_LIBRARIES ${GIT2_LIBRARY} z ${OPENSSL_LIBRARIES})
 endif()
 
 mark_as_advanced(
